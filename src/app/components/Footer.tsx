5cb66ffa383e27ad34c6248c5cb2c8ce8a2b57f2
Max Lindquist <ml227cu@student.lnu.se>
2025-10-24 01:34:30 +0200

    feat: implement TextCard component and update About and Home pages with TextCard usage for improved layout

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -4,14 +4,16 @@ import Image from 'next/image';
 export default function Footer() {
     return (
         <footer className="flex items-center bg-gray-800 text-white p-8 mt-12">
-          <p className="text-left">&copy; 2025 Max Lindquist</p>
-          <div className="ml-auto flex space-x-4">
-          <Link target="_blank" href="https://github.com/maxlindquistt">
-                <Image src="/github-mark-white.svg" alt="GitHub" width={30} height={30} />
-          </Link>
-          <Link target="_blank" href="https://www.linkedin.com/in/maxlindquistt/">
-                <Image src="/linkedin-svgrepo-com.svg" alt="LinkedIn" width={30} height={30} />
-          </Link>
+          <div className="max-w-4xl mx-auto flex w-full">
+            <p className="text-left">&copy; 2025 Max Lindquist</p>
+            <div className="ml-auto flex space-x-4">
+            <Link target="_blank" href="https://github.com/maxlindquistt">
+                    <Image src="/github-mark-white.svg" alt="GitHub" width={30} height={30} />
+            </Link>
+            <Link target="_blank" href="https://www.linkedin.com/in/maxlindquistt/">
+                    <Image src="/linkedin-svgrepo-com.svg" alt="LinkedIn" width={30} height={30} />
+            </Link>
+            </div>
           </div>
         </footer>
     );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,58 @@
 import Link from "next/link";
+import TextCard from "./components/TextCard";
 
 export default function HomePage() {
     return (
-        <main className="max-w-7xl mx-auto p-6 grid grid-cols-1 md:grid-cols-2 gap-8 items-center min-h-[70vh]">
-          <div>
-            <h1 className="text-4xl font-bold mb-4">Hi, I'm Max Lindquist</h1>
-            <p className="text-lg mb-2">
-                I craft fast, scalable, and user-focused web applications using modern technologies like React, Next.js, Node.js, and TypeScript. <br /> <br />
-                Explore my projects and get in touch to see how we can build something great together! <br />
-            </p>
+        <main className="text-center md:text-left mx-auto p-6 md:max-h-screen flex flex-wrap gap-6 justify-center">
+
+          <div className="h-fit float-left">
+            <TextCard>
+              <h1 className="text-4xl font-bold mb-4">Hi, I'm Max Lindquist</h1>
+              <p className="text-lg mb-2">
+                  I craft fast, scalable, and user-focused web applications using modern technologies like React, Next.js, Node.js, and TypeScript.
+              </p>
+              <p className="text-lg mb-2">
+                Explore my projects and get in touch to see how we can build something great together!
+              </p>
             <Link href="/projects" className="text-blue-200 text-lg hover:underline">View My Projects!</Link> <br />
             <Link href="/contact" className="text-blue-200 text-lg hover:underline">Contact Me!</Link>
+            </TextCard>
           </div>
-          <div>
-            <h2 className="text-2xl font-bold mb-4">A Little About Me</h2>
-            <p className="text-lg mb-2">
-              I'm a full-stack developer with a passion for turning ideas into functional, elegant software. <br />
-              From front-end interfaces that delight users to back-end systems that scale, I love solving complex problems through clean code and thoughtful design.
-            </p>
-            <Link href="/about" className="text-blue-200 text-lg hover:underline">See My Tech Stack And Learn More About Me!</Link>
+
+          <div className="h-fit float-right md:mt-55">
+            <TextCard>
+              <h2 className="text-2xl font-bold mb-4">A Little About Me</h2>
+              <p className="text-lg mb-2">
+                I'm a full-stack developer with a passion for turning ideas into functional, elegant software.
+              </p>
+              <p className="text-lg mb-2">
+                From front-end interfaces that delight users to back-end systems that scale, I love solving complex problems through clean code and thoughtful design.
+              </p>
+              <Link href="/about" className="text-blue-200 text-lg hover:underline">See My Tech Stack And Learn More About Me!</Link>
+            </TextCard>
+  
           </div>
-          
+          <div className="h-fit float-left md:-mt-20">
+            <TextCard>
+              <h2 className="text-2xl font-bold mb-4">Let's Connect</h2>
+              <p className="text-lg mb-2">
+                Whether you're interested in collaborating on a project, have a question about my work, or just want to say hi, I'd love to hear from you.
+              </p>
+              <p className="text-lg mb-2">
+                Feel free to reach out via email or connect with me on LinkedIn or GitHub.
+              </p>
+              <Link href="/contact" className="text-blue-200 text-lg hover:underline">Get In Touch!</Link>
+            </TextCard>
+          </div>
+
+          <div className="h-fit float-right md:mt-20">
+            <TextCard>
+              <h2 className="text-2xl font-bold mb-4">Currently Studying</h2>
+              <p className="text-lg mb-2">
+                As I'm currently studying Web Programming at Linnaeus University in Kalmar, Sweden, where I'm deepening my knowledge of web development and honing my skills, this site also serves as a platform to showcase what I've learned and the projects I've built along the way.
+              </p>
+            </TextCard>
+          </div>
         </main>
     );
 }
