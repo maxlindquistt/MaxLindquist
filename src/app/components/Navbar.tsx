58d52a3f087e68dfd8c979883e113adfe4cac382
Max Lindquist <ml227cu@student.lnu.se>
2025-10-23 20:20:38 +0200

    feat: update Navbar component with LinkedIn and github icon and improve layout

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,13 +1,24 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Navbar() {
     return (
         <nav className="bg-gray-950 shadow-md p-6 mb-12">
-          <div className="max-w-6xl mx-auto flex gap-6">
+          <div className="max-w-6xl mx-auto flex">
+            <div className="ml-0 gap-6 flex">
             <Link href="/" className="font-bold">Home</Link>
             <Link href="/projects">Projects</Link>
             <Link href="/about">About</Link>
             <Link href="/contact">Contact</Link>
+            </div>
+            <div className="ml-auto gap-6 flex">
+                <Link href="https://github.com/maxlindquistt">
+                <Image src="/github-mark-white.svg" alt="GitHub" width={30} height={30} />
+                </Link>
+                <Link href="https://www.linkedin.com/in/maxlindquistt/">
+                <Image src="/linkedin-svgrepo-com.svg" alt="LinkedIn" width={30} height={30} />
+                </Link>
+            </div>
           </div>
         </nav>
     );
