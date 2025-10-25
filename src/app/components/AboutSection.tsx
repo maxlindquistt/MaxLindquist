3a91288f100c128e3f6e55f1f0baf46132500b77
Max Lindquist <ml227cu@student.lnu.se>
2025-10-25 21:15:59 +0200

    smaller font in about

--- src/app/components/AboutSection.tsx
+++ src/app/components/AboutSection.tsx
@@ -8,7 +8,7 @@ export default function AboutSection() {
     return (
         <section className="grid grid-cols-2 items-center ">
 
-            <h2 className="text-3xl font-bold text-center mb-6 col-span-2">About Me</h2>
+            <h2 className="text-2xl font-bold text-center mb-6 col-span-2">About Me</h2>
             <Image src="/about.jpg" alt="Profile Picture" width={150} height={150} className="rounded mx-auto" />
     
             <div className="max-w-[150px] mx-auto text-center lg:text-left lg:text-lg lg:max-w-none">
